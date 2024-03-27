12a218916f3fac69b75531a15035873935140720
Matthew <codemat.biz@gmail.com>
2024-03-27 11:31:42 +0100

    updated

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -2,19 +2,14 @@
 
 import DashboardNav from "@/components/DashboardNav";
 import SideNav from "@/components/SideNav";
-import { Contact, Film, UserRound, Users } from "lucide-react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 const DashboardLayout = ({ children }) => {
-
-
   return (
     <div className='flex'>
-     <SideNav />
+      <SideNav />
       <div className='px-4 flex-grow transition-all transform duration-500  min-h-screen md:ml-[200px]'>
         <DashboardNav />
-      <main>{children}</main>  
+        <main>{children}</main>
       </div>
     </div>
   );
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,9 +3,9 @@ import { list } from "@/list";
 
 const Dashboard = () => {
   return (
-    <div className='w-full min-h-screen max-w-3xl mx-auto'>
+    <div className='w-full min-h-screen max-w-3xl mx-auto '>
       <h1 className='text-center text-3xl md:text-4xl'>DataDeck</h1>
-      <div className='mt-8'>
+      <div className='my-8'>
         {/* <pre>{ JSON.stringify(list, null, 2)}</pre> */}
         <StaffDataList staffList={list} />
       </div>
