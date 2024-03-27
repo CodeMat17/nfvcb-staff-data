08051e1d57bddb152abd1a96e8a976a2f5fe6572
Matthew <codemat.biz@gmail.com>
2024-03-27 11:09:40 +0100

    updated

--- components/DashboardNav.js
+++ components/DashboardNav.js
@@ -4,7 +4,7 @@ import ThemeToggleButton from "./ThemeToggleButton";
 
 const DashboardNav = () => {
   return (
-    <div className='sticky top-0 z-50 px-4 py-1.5 flex items-center justify-between mb-8 bg-white dark:bg-slate-950'>
+    <div className='sticky top-0 z-50 py-1.5 flex items-center justify-between mb-8 bg-white dark:bg-slate-950'>
       <div className='relative md:hidden w-[60px] aspect-square rounded-lg overflow-hidden'>
         <Image alt='' priority fill src='/nfvcb_logo.webp' />
       </div>
